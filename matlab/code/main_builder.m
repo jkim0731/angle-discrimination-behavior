cd7ed176ff5c03d9225d3b47259de1e896925ccc
Jonathan Cheung <jacheung6@gmail.com>
2019-04-04 14:06:43 -0700

    Added feature that allows for using each individual touch to discriminate
    in main_builder
    - this is dislayed under 'Xhow'
    - this is taken into account under the designMatrixBuilder function a couple lines along hte main script.
    - can run with heaviestCoefficients_fineAngle to find heaviest feature

--- matlab/code/main_builder.m
+++ matlab/code/main_builder.m
@@ -5,18 +5,20 @@ mns = [{'JK025'},{'JK027'},{'JK030'},{'JK036'},{'JK039'},{'JK052'}];
 
 % sns = [{'S05'},{'S02'},{'S04'},{'S02'},{'S02'},{'S05'}]; %naive
 % sns = [{'S18'},{'S07'},{'S20'},{'S16'},{'S21'},{'S20'}]; %expert
-sns = [{'S04'},{'S03'},{'S03'},{'S01'},{'S01'},{'S03'}]; %naive discreteAngles
-% sns = [{'S19'},{'S16'},{'S21'},{'S17'},{'S23'},{'S25'}]; %expert discreteAngles
+% sns = [{'S04'},{'S03'},{'S03'},{'S01'},{'S01'},{'S03'}]; %naive discreteAngles
+sns = [{'S19'},{'S16'},{'S21'},{'S17'},{'S23'},{'S25'}]; %expert discreteAngles
 % sns = [{'S22'},{'S14'},{'S22'},{'S18'},{'S24'},{'S26'}]; %radial distance
 
-mdlName = 'mdlDiscreteNaiveChoice';
+mdlName = 'mdlDiscreteIndivTouch';
 whiskDir = 'protraction'; % 'protraction' or 'all' to choose which touches to use
 touchOrder = 'all'; % 'first' or 'all' to choose which touches pre-decision
-yOut = 'choice'; % can be 'ttype' (45 vs 135), 'discrete' (45:15:135) or 'choice' (lick right probability)
+yOut = 'discrete'; % can be 'ttype' (45 vs 135), 'discrete' (45:15:135) or 'choice' (lick right probability)
+Xhow = 'individual'; %can be 'mean' or 'individual' so each touch is counted 
+
 
 groupMdl = cell(length(mns),1); 
 
-if strcmp(touchOrder,'first')
+if strcmp(touchOrder,'first') || strcmp(Xhow,'individual')
     DmatSelect = [1:6 8:17]; %tossing touch counts since it is all ones
 else
     DmatSelect = [1:17]; %feats from 1:21
@@ -64,7 +66,7 @@ for d = 1:length(sns)
 %     featurePlotter(it,dt,outcomes,yOut)
     
     %% Design matrix construction
-    [DmatXIT, DmatXDT, fieldsList] = designMatrixBuilder(it,dt);
+    [DmatXIT, DmatXDT, tnums, fieldsList] = designMatrixBuilder(it,dt,Xhow);
 
     DmatX = [DmatXIT DmatXDT];
 
@@ -72,21 +74,41 @@ for d = 1:length(sns)
     fieldsList = fieldsList(DmatSelect);
 
     % Dmat Y builder
-    if strcmp(yOut,'ttype')
-        DmatY = (outcomes.matrix(1,:)==1)';
-    elseif strcmp(yOut,'choice')
-        DmatY = (outcomes.matrix(3,:)==1)'; 
-    elseif strcmp(yOut,'discrete')
-        DmatY = (outcomes.matrix(6,:))';
+    if strcmp(Xhow,'mean')
+        if strcmp(yOut,'ttype')
+            DmatY = (outcomes.matrix(1,:)==1)';
+        elseif strcmp(yOut,'choice')
+            DmatY = (outcomes.matrix(3,:)==1)';
+        elseif strcmp(yOut,'discrete')
+            DmatY = (outcomes.matrix(6,:))';
+        end
+        missY = outcomes.matrix(5,:) == -1;
+    elseif strcmp(Xhow,'individual')
+        if strcmp(yOut,'ttype')
+            tmpY = (outcomes.matrix(1,:)==1)';
+        elseif strcmp(yOut,'choice')
+            tmpY = (outcomes.matrix(3,:)==1)';
+        elseif strcmp(yOut,'discrete')
+            tmpY = (outcomes.matrix(6,:))';
+        end
+        misstmp = outcomes.matrix(5,:) == -1;
+        missY = misstmp(tnums);
+        DmatY = tmpY(tnums); 
+    else
+        error('need to define "Xhow" as "mean" or "individual"') 
     end
+        
     
-    %removing nan values
-    missTrials = find(outcomes.matrix(5,:) == -1)';
+    %removing nan(non-touch) values and miss
+    missTrials = find(missY == 1)';
     [rowsNAN, ~] = find(isnan(DmatX));
     [rowsINF, ~] = find(isinf(DmatX));
     DmatY(unique([rowsNAN ; rowsINF ; missTrials]),:)=[];
     DmatX(unique([rowsNAN ; rowsINF ; missTrials]),:)=[];
+    
+    if strcmp(Xhow,'mean')
     outcomes.matrix(7,unique([rowsNAN ; rowsINF ; missTrials])) = 0; %indexing non-touch and miss trials
+    end
     
     %standardization
     DmatX = (DmatX-mean(DmatX))./std(DmatX);
